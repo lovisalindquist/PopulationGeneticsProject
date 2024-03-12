f34954bd760a49669b9becd75784653a8e342381
lovisalindquist <lovisalindquist@users.noreply.github.com>
2024-03-12 08:57:19 +0100

    Update scripts

--- Scripts/CircosPlotterApp.R
+++ Scripts/CircosPlotterApp.R
@@ -1,3 +1,19 @@
+
+" This script loads the parsed data file and visualises different types of plots using an R Shiny interface.
+Two main functions are used: 
+  circos_plotter_all - plots all connections between all individuals or populations, highlights the connections associated with selected individuals or populations
+  circos_plotter - takes a main individual or population of interest and plots the connections to chromosomes for a number of selected individuals or populations. 
+
+The application is developed using R shiny. The function defining the user interface contains several tabs, graphs and tables that are updated in the server. 
+The server observes input from the user and updates the seleections and graphs accordingly.
+
+
+Author: Lovisa Lindquist
+Date: 2024-03-12
+"
+
+########################################################################################################################3
+
 #install libraries
 if (!require("circlize")) install.packages("circlize")
 if (!require("shiny")) install.packages("shiny")
@@ -23,7 +39,7 @@ circos_plotter_all <- function(input_data, selection, level) {
   color <- c()
   #if at individual level, shorten the data set to reduce computational burden, ideally the entire data set should be used
   if (level == "Individual-Individual") {
-    short_data <- input_data[(1:100),]
+    short_data <- input_data[1:100,]
     #identify all unique individuals in the data
     unique_id1 <- unique(short_data$ID1) 
     unique_id2 <- unique(short_data$ID2)
@@ -32,18 +48,18 @@ circos_plotter_all <- function(input_data, selection, level) {
       subdata <- short_data[short_data$ID1 == id1,]
       for (id2 in unique_id2) {
         subsubdata <- subdata[subdata$ID2 == id2,]
-        if (length(subsubdata$ID1) > 0) #if there is data, extract the necessary details and assign a colour.
+        if (length(subsubdata$ID1) > 0) {#if there is data, extract the necessary details and assign a colour.
           ID1 <- rbind(ID1, id1)
           ID2 <- rbind(ID2, id2)
           cM <- rbind(cM, sum(subsubdata$cM))
           color <- rbind(color, ifelse(id1 %in% selection, yes = rainbow(500)[which(selection == id1)*50], no = ifelse(id2 %in% selection, yes = rainbow(500)[which(selection == id2)*50], no = "grey")))
-          
+        }
         }
       }
   }
   # if the comparison is at population level, go through all populations and generate a temporary data table
     else if (level == "Population-Population") {
-      input_data <- input_data[1:"1000",]
+      input_data <- input_data[1:1000,]
     unique_id1 <- unique(input_data$ID1_Group)
     unique_id2 <- unique(input_data$ID2_Group)
     for (id1 in unique_id1) {
@@ -72,7 +88,6 @@ circos_plotter_all <- function(input_data, selection, level) {
   circos.clear() #make sure old graphs are cleared
   # call chord diagram function, all grids specified as grey, colors based on previously generated matrix
   chordDiagram(finaldata, grid.col="grey", preAllocateTracks = 0.5, symmetric = F, col = finaldata$color) 
-  
 }
 
 # define a function that takes an selected input subject (individual/group) and one or more selected comparisons, creates a chord diagram
@@ -146,9 +161,10 @@ circos_plotter <- function(input_data, subject, comparison, level) {
   }
   
   circos.clear() #make sure old graphs are cleared
-  # call chord diagram function, all grids specified as grey, colors based on previosuly generated matrix
+  # call chord diagram function, all grids specified as grey, colors based on previously generated matrix
   chordDiagram(mat, grid.col="grey", preAllocateTracks = 0.5, col = col_mat)  
   title(paste("\nSelected:", subject)) # add a title with the reference subject name
+  return(col_mat)
 }
 
 # define function for user interface
@@ -184,11 +200,11 @@ server <- function(input, output, session) {
   # define new options in widget based on previous selections
   observeEvent(input$plot, updateSelectInput(session, "level", "Level", if (input$plot == "All at once") {choices = c("Individual-Individual", "Population-Population")} else if (input$plot == "One to many") {choices = c("Individual-Individual", "Population-Population", "Individual-Population")}, selected = "Individual-Individual"))
   observeEvent(c(input$level, input$plot), updateSelectizeInput(session, "reference", "Selection 1", if (input$plot == "One to many" & input$level == "Individual-Individual") {choices = sort(unique(dat$ID1))} else if (input$plot == "One to many" & input$level == "Population-Population") {choices = sort(unique(dat$ID1_Group))} else if (input$level == "Individual-Population") {choices = sort(unique(dat$ID1))} else {choices = c("NA")}, if (input$plot == "One to many" & input$level == "Population-Population") {selected = sort(unique(dat$ID1_Group))[1]} else if (input$plot == "One to many" & input$level == "Individual-Individual") {selected = sort(unique(dat$ID1))[1]} else {selected = "NA"}))
-  observeEvent(c(input$plot, input$level), updateCheckboxGroupInput(session, "comparison", "Selection 2", if (input$plot == "All at once" & input$level == "Individual-Individual") {choices = sort(unique(dat$ID2))} else if (input$plot == "All at once" & input$level == "Population-Population") {choices = sort(unique(dat$ID2_Group))} else if (input$plot == "One to many" & input$level == "Individual-Individual") {choices = sort(unique(dat$ID2[dat$ID1 == input$reference]))} else if (input$plot == "One to many" & input$level == "Population-Population") {choices = sort(unique(dat$ID2_Group[dat$ID1_Group == input$reference]))} else if (input$plot == "One to many" & input$level == "Individual-Population") {choices = sort(unique(dat$ID2_Group[dat$ID1 == input$reference]))}))
+  observeEvent(c(input$plot, input$level, input$reference), updateCheckboxGroupInput(session, "comparison", "Selection 2", if (input$plot == "All at once" & input$level == "Individual-Individual") {choices = sort(unique(dat$ID2))} else if (input$plot == "All at once" & input$level == "Population-Population") {choices = sort(unique(dat$ID2_Group))} else if (input$plot == "One to many" & input$level == "Individual-Individual") {choices = sort(unique(dat$ID2[dat$ID1 == input$reference]))} else if (input$plot == "One to many" & input$level == "Population-Population") {choices = sort(unique(dat$ID2_Group[dat$ID1_Group == input$reference]))} else if (input$plot == "One to many" & input$level == "Individual-Population") {choices = sort(unique(dat$ID2_Group[dat$ID1 == input$reference]))}))
   observeEvent(input$plot, updateTabsetPanel(session, "tabs", if (input$plot == "All at once") {selected = "All at once"} else if (input$plot == "One to many") {selected = "One to many"} else {selected = "All at once"}))
   
   # identify which type of plot should be used based on plot type and level
-  observeEvent(c(input$plot, input$level),
+  observeEvent(c(input$plot, input$level, input$comparison),
                if (input$plot == "All at once") { #if all at once should be compared
                  output$circos_1 <- renderPlot({
                    validate(
@@ -229,8 +245,11 @@ server <- function(input, output, session) {
                        Total = total,
                        InferredRelation = IR
                      )
+                     
                      # create drop down menu with detailed data
-                     reactable(summary_data, details = function(index) {
+                     reactable(summary_data, columns = list(
+                       color = colDef(name = "", style = c("red", "blue"))
+                     ),details = function(index) {
                        data <- dat[c(dat$ID1 == input$reference & dat$ID2 == summary_data$ID2[index]), c(1:2, 10, 3:4, 11, 5:7,9)]
                        data$cM <- round(data$cM, 3)
                        reactable(data, outlined = T)
@@ -243,13 +262,17 @@ server <- function(input, output, session) {
                        total <- rbind(total, round(sum(as.numeric(tempdata$cM)),3))
                        avg <- rbind(avg, c(round(sum(as.numeric(tempdata$cM))/length(tempdata$cM),3)))
                      }
+                     
+                    
                      # append all saved data to summary table
-                     summary_data <- tibble(
+                     summary_data <- data.frame(
                        ID1 = ID1,
                        ID2 = ID2,
+                      # color = c(rep("O", length(ID1))),
                        Total = total,
                        Average = avg
-                     ) 
+                     )
+                     
                      # create drop down menu with detailed data
                      reactable(summary_data, details = function(index) {
                        data <- dat[c(dat$ID1_Group == input$reference & dat$ID2_Group == summary_data$ID2[index]), c(1:2, 10, 3:4, 11, 5:7,9)] 
@@ -282,8 +305,5 @@ server <- function(input, output, session) {
   
 # call application
 shinyApp(ui, server)
-
-
-
 
 
